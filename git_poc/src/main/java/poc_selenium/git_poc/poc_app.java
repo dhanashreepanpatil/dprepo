150bf64fd6e40a751d3963674f9111c61c5d5bc7
dhanashreepanpatil <dhanashree.panpatil@flexur.com>
2019-09-08 16:53:18 +0530

    Init project setup

---
+++ git_poc/src/main/java/poc_selenium/git_poc/poc_app.java
@@ -0,0 +1,32 @@
+package poc_selenium.git_poc;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class poc_app {
+	WebDriver driver;
+	@Test
+	public void validLogin() throws Exception {
+	System.setProperty("webdriver.chrome.driver", "D:\\Dhanashree\\Selenium\\chromedriver.exe");	
+	driver = new ChromeDriver();
+
+	String eTitle = "Meet Guru99";
+	String aTitle = "";
+	
+	driver.get("http://guru99.com");
+	driver.manage().window().maximize();
+	
+	aTitle=driver.getTitle();
+	if(aTitle.contentEquals(eTitle))
+	{
+		System.out.println("Test passed!");
+	}
+	else
+	{
+		System.out.println("Test Failed!, Try again");
+	}
+	driver.close();
+	}	
+}
+
